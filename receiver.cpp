4a322dcfec3954327b5e4996d6458e85378efca7
mjokfox <mjokfox@hotmail.com>
2025-04-10 09:24:21 +0200

    receiver code stuffs

---
+++ receiver.cpp
@@ -0,0 +1,132 @@
+#include <arpa/inet.h> // For sockaddr_in and inet_ntoa()
+#include <csignal>
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <sys/socket.h> // For socket functions
+#include <unistd.h>     // For close()
+
+#define MIC_AMOUNT 4
+#define RAW_MIC_LENGTH 360
+#define MIC_LENGTH 120
+
+int sockfd; // Socket file descriptor
+
+void handle_sigint(int sig) {
+  // Close the socket and exit
+  close(sockfd);
+  std::cout << "\nServer terminated gracefully" << std::endl;
+  exit(0);
+}
+
+void process_mic(char* data, size_t n) {
+    if (!(data[0] == 'A' && data[1] == 'U' && data[2] == 'D' && data[3] == 'I')){
+        return;
+    }
+    char seq = data[4];
+
+    char* raw_data = (char*) malloc(n - 5);
+    memcpy(raw_data, &data[5],n-5);
+    
+
+    char** raw_mic_data = (char**) malloc(MIC_AMOUNT * sizeof(char*));
+    for (int i = 0; i < MIC_AMOUNT; i++) {
+        raw_mic_data[i] = (char*) malloc(RAW_MIC_LENGTH);
+    }
+
+    uint16_t offset = 0;
+    for(int i = 0; i < MIC_AMOUNT; i++) {
+        memcpy(raw_mic_data[i], &raw_data[++offset], RAW_MIC_LENGTH);
+        offset += RAW_MIC_LENGTH;
+    }
+    free(raw_data);
+
+    int32_t** mic_data = (int32_t**) malloc(MIC_AMOUNT * sizeof(int32_t*));
+    for (int i = 0; i < MIC_AMOUNT; i++) {
+        mic_data[i] = (int32_t*) malloc(MIC_LENGTH * sizeof(int32_t*));
+    }
+
+    for(int j = 0; j < MIC_AMOUNT; j++) {
+        char* rmd = raw_mic_data[j];
+        for(int i = 0; i < RAW_MIC_LENGTH; i += 3) {
+            mic_data[j][i/3] = (rmd[i+2] << 16 |  rmd[i+1] << 8 | rmd[i]) | (rmd[i+2] >> 7 == 0x01 ? 0xFF000000 : 0x00000000);
+        }
+    }
+
+    
+    for(int i = 0; i < MIC_AMOUNT; i++) {
+        free(raw_mic_data[i]);
+    }
+    free(raw_mic_data);
+
+
+    for(int j = 0; j < MIC_AMOUNT; j++) {
+        std::cout << j << ": [";
+        for(int i = 0; i < MIC_LENGTH-1; i++) {
+            std::cout << mic_data[j][i] << ", ";
+        }
+        std::cout << mic_data[j][MIC_LENGTH-1] << "]" << std::endl;
+    }
+    
+
+    for(int i = 0; i < MIC_AMOUNT; i++) {
+        free(mic_data[i]);
+    }
+    free(mic_data);
+    return;
+}
+
+int main() {
+  struct sockaddr_in server_addr, client_addr;
+  char buffer[2048];
+  socklen_t addr_len = sizeof(client_addr);
+
+  // Set up the signal handler for Ctrl+C (SIGINT)
+  signal(SIGINT, handle_sigint);
+
+  // Create a UDP socket
+  if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+    perror("Socket creation failed");
+    exit(EXIT_FAILURE);
+  }
+
+  // Zero out the server address structure
+  memset(&server_addr, 0, sizeof(server_addr));
+
+  // Set up the server address
+  server_addr.sin_family = AF_INET;         // IPv4
+  server_addr.sin_addr.s_addr = INADDR_ANY; // Bind to all interfaces
+  server_addr.sin_port = htons(3333);       // Port 8081
+
+  // Bind the socket to the server address
+  if (bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) <
+      0) {
+    perror("Bind failed");
+    close(sockfd);
+    exit(EXIT_FAILURE);
+  }
+
+  std::cout << "C++ UDP server listening on port 3333" << std::endl;
+
+  while (true) {
+    // Receive a message from a client
+    ssize_t n = recvfrom(sockfd, buffer, sizeof(buffer) - 1, 0,
+                         (struct sockaddr *)&client_addr, &addr_len);
+    if (n < 0) {
+      perror("Receive failed");
+      continue;
+    }
+
+    buffer[n] = '\0'; // Null-terminate the received data
+
+    // Print the received message
+    process_mic(buffer, n);
+    // std::cout << "C++ server received: " << buffer << std::endl;
+  }
+
+  // Close the socket (unreachable code in this example)
+  close(sockfd);
+
+  return 0;
+}
--- receiver.py
+++ receiver.py
@@ -1,11 +1,27 @@
 import struct
 from socket import *
+from ctypes import c_int32
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+N_SAMPLES = 120  # number of samples per mic (from each UDP packet)
+x = np.arange(N_SAMPLES)
+
+fig, axs = plt.subplots(4, 1, figsize=(10, 6), sharex=True)
+lines = [ax.plot(x, np.zeros(N_SAMPLES))[0] for ax in axs]
+[ax.set_ylabel(f"Mic {i+1}") for i, ax in enumerate(axs)]
+axs[-1].set_xlabel("Sample index")
+plt.ion()
+plt.show()
 
 sock = socket(AF_INET, SOCK_DGRAM)
 sock.bind(("0.0.0.0", 3333))
 
+MIC_AMOUNT = 4
+
 while True:
-    data, addr = sock.recvfrom(65535)
+    data, addr = sock.recvfrom(2048)
 
     # Parse header
     header = data[0:4]
@@ -13,21 +29,31 @@ while True:
         continue  # Invalid packet
 
     seq = data[4]  # Sequence number
-    ptr = 5  # Pointer to start of mic data
 
-    mics = {}
-    while ptr < len(data):
-        # Read identifier and length
-        mic_id = chr(data[ptr])
-        ptr += 1
-        (mic_len,) = struct.unpack(">H", data[ptr:ptr + 2])
-        ptr += 2
 
-        # Extract audio data (24-bit samples)
-        mic_data = data[ptr:ptr + mic_len]
-        ptr += mic_len
+    raw_data = data[5:]
+    raw_length = (len(raw_data) - MIC_AMOUNT) // MIC_AMOUNT
 
-        # Store in dictionary
-        mics[mic_id] = mic_data
+    raw_mic_data = []
+    offset = 0
+    for i in range(MIC_AMOUNT):
+        offset += 1
+        raw_mic_data.append(raw_data[offset : offset + raw_length])
+        print(offset)
+        offset += raw_length
 
-    # Now process mics['1'], mics['2'], etc.
+    mic_data = []
+    for j in range(MIC_AMOUNT):
+        mic_data.append([])
+        rmd = raw_mic_data[j]
+        for i in range(0, len(rmd), 3):
+            mic_data[j].append(c_int32((rmd[i+2] << 16 | rmd[i+1] << 8 | rmd[i]) | (0xFF000000 if rmd[i+2] >> 7 == 0x01 else 0x00000000)).value)
+
+    # print(mic_data)
+        # Update plots
+    for i in range(MIC_AMOUNT):
+        lines[i].set_ydata(mic_data[i])
+        axs[i].relim()
+        axs[i].autoscale_view()
+
+    plt.pause(0.001)  # Allow the plot to update
